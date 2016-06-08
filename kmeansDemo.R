7b3dc515c44f9c772bba47966c63d065b2bc8a05
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-06-09 07:50:31 +0800

    Added Hierarchical Clustering Demo, too

--- kmeansDemo.R
+++ kmeansDemo.R
@@ -1,3 +1,5 @@
+## k-means Demo
+
 x <- seq(from=1,to=10,length.out = 100)
 y1 <- rnorm(50, 4, 0.5)
 y2 <- rnorm(50, 8, 0.5)
@@ -6,3 +8,11 @@ plot(x,y)
 obs <- data.frame(x,y)
 kmeansObj <-  kmeans(obs, centers = 2)
 plot(x,y, col= kmeansObj$cluster)
+
+## Hierarchical Clustering
+
+distM <- dist(obs)
+clusters <- hclust(distM, method = "ward.D")
+plot(clusters)
+mem <- cutree(clusters, k =2)
+plot(x,y, col=mem)
